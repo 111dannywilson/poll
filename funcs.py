243a216fba773d1bbf435aefc6e8a86815769624
Danny Wilson <111dannywilson@gmail.com>
2023-08-17 12:39:31 -1200

    first commit

--- funcs.py
+++ funcs.py
@@ -85,33 +85,3 @@ all_options = []
 selected_options = []
 counter = {}
 number_of_votes = 0
-
-
-a = 5
-print((2.9 / 100) * 3)
-# def isPhoneNumber(text):
-#     if len(text) != 12:
-#         return False
-#     for i in range(0, 3):
-#         if not text[i].isdecimal():
-#             return False
-#     if text[3] != "-":
-#         return False
-#     for i in range(4, 7):
-#         if not text[i].isdecimal():
-#             return False
-#     if text[7] != "-":
-#         return False
-#     for i in range(8, 12):
-#         if not text[i].isdecimal():
-#             return False
-#     return True
-
-
-# message = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
-# message = message.split(" ")
-# phone_number = None
-# for text in message:
-#     if isPhoneNumber(text):
-#         phone_number = text
-# print(f"Phone number is {phone_number}")
